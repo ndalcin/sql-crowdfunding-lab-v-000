f2937faa8620bedb7c15ec3aaf2591c3cfee16e8
ndalcin <dalcin.nicole@gmail.com>
2017-10-03 09:01:30 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,9 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 FROM pledges
 INNER JOIN projects
 ON pledges.project_id = projects.id
-GROUP BY projects.title
-ORDER BY projects.title
-;"
+GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
@@ -21,8 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 FROM pledges
 JOIN users
 ON pledges.user_id = users.id
-GROUP BY users.name
-ORDER BY users.name"
+GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -31,7 +28,6 @@ FROM pledges
 JOIN projects
 ON pledges.project_id = projects.id
 GROUP BY projects.title HAVING amount_over >= 0"
-#GROUP BY projects.title HAVING SUM(pledges.amount) > projects.funding_goal
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
@@ -46,9 +42,9 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount
 FROM pledges
-LEFT JOIN projects
+JOIN projects
 ON pledges.project_id = projects.id
-GROUP BY pledges.id HAVING projects.category = 'music'"
+WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
